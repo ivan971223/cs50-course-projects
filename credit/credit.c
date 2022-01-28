3b58dff4b41cd52be8d2373156c828d7e7d8cf78
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-01-28 18:16:49 +0000

    Fri, Jan 28, 2022, 6:16 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,6 +7,7 @@ int get_digit_num(long num);
 int main(void)
 {
     long num;
+    int checksum = 0;
 
     do
     {
@@ -14,23 +15,25 @@ int main(void)
     } while (num < 0);
 
     int digit = get_digit_num(num);
-    if (digit < 13 || digit > 16 || digit==14)
+    if (digit < 13 || digit > 16 || digit == 14)
     {
-        printf("INVALID");
+        printf("INVALID\n");
     }
 
-    // while (i < 1)
-    // {
+    for (int i = 0; i < digit; i++)
+    {
+        if (i % 2 == 1)
+        {
+            double divided_num = num / 10.00;
+            long temp_num = divided_num;
+            int digit = (divided_num - temp_num) * 10;
+            printf("%i\n", digit);
+            checksum += digit;
+        }
+        printf("%f\n", divided_num);
+        printf("%li\n", temp_num);
 
-    //     double divided_num = num / 10.00;
-    //     long temp_num = divided_num;
-    //     int digit = (divided_num - temp_num) * 10;
-
-    //     printf("%f\n", divided_num);
-    //     printf("%li\n", temp_num);
-    //     printf("%i\n", digit);
-    //     i++;
-    // }
+    }
 }
 
 int get_digit_num(long num)
