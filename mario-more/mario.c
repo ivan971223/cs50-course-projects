f9ab9163bde1b9a140bcf4efa2ac4d3f87867304
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-01-28 16:44:33 +0000

    Fri, Jan 28, 2022, 4:44 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,1 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    do
+    {
+        int h = get_int("Height: ");
+    } while (h < 1 || h > 8)
+
+    for(int i=0; i<h; i++)
+    {
+        
+    }
+}
