9e08661b1f1c6cb798e0256fa9d7a3a46e042a47
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-01-30 16:42:57 +0000

    Sun, Jan 30, 2022, 4:42 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -68,18 +68,14 @@ bool vote(string name)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidates[i].name)==0)
+        if (strcmp(name, candidates[i].name) == 0)
         {
             candidates[i].votes++;
             return true;
         }
-        else
-        {
-            return false;
-        }
     }
 
-    return true;
+    return false;
 }
 
 // Print the winner (or winners) of the election
