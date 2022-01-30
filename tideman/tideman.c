29e04d015e9de73be88337e9da231e61bf7de6dd
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-01-30 17:48:28 +0000

    Sun, Jan 30, 2022, 5:48 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -117,9 +117,9 @@ void record_preferences(int ranks[])
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = 1; j < candidate_count; j++)
         {
-            preferences[i][j]
+            preferences[i][j] 
         }
     }
     return;
