ab10a121684fbc28d3df868de5c649a06501d13c
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-03 11:02:48 +0000

    Thu, Feb 3, 2022, 11:02 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -211,7 +211,7 @@ void lock_pairs(void)
     }
     for (int i = 0; i < pair_count; i++)
     {
-        printf("%i%i", locked[pairs[i].winner], locked[pairs[i].loser]);
+        printf("%b", locked[pairs[i].winner]locked[pairs[i].loser]);
     }
     return;
 }
