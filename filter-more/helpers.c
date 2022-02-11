e99171f7ae7867ab9af20943cf56f1136f37fa39
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-11 15:38:47 +0000

    Fri, Feb 11, 2022, 3:38 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -43,6 +43,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
     return;
 }
 
