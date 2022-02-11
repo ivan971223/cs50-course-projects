b5b57da6a86063778dfccb900cf0bd1a521bc53e
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-11 10:13:15 +0000

    Fri, Feb 11, 2022, 10:13 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -32,41 +32,28 @@ int main(int argc, char *argv[])
     }
 
     float factor = atof(argv[3]);
-    FILE *file = fopen("1.txt", "a");
-    FILE *file2 = fopen("2.txt", "a");
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
     fread(header, sizeof(uint8_t), HEADER_SIZE, input);
     fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
-    for (int i = 0; i < HEADER_SIZE; i++)
-    {
-        // fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
-        printf("%u", header[i]);
-        fprintf(file, "%u\n", header[i]);
-    }
-
-    uint8_t k;
-    while (fread(&k, sizeof(uint8_t), 1, output))
-    {
-        fprintf(file2, "%u\n", k);
-    }
-    // TODO: Read samples from input file and write updated data to output file
-    // int16_t buffer;
-    // int i = 0;
-    // while (fread(&buffer, sizeof(int16_t), 1, input))
+    // for (int i = 0; i < HEADER_SIZE; i++)
     // {
-    //     if (i > 21)
-    //     {
-    //         buffer *= factor;
-    //         printf("%u\n", buffer);
-    //         fwrite(&buffer, sizeof(int16_t), 1, output);
-    //     }
-    //     i++;
+    //     // fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
+    //     printf("%u", header[i]);
+    //     fprintf(file, "%u\n", header[i]);
     // }
+
+    // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    int i = 0;
+    while (fread(&buffer, sizeof(int16_t), 1, input))
+    {
+            buffer *= factor;
+            printf("%u\n", buffer);
+            fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
 
     // Close files
     fclose(input);
     fclose(output);
-    fclose(file);
-    fclose(file2);
 }
