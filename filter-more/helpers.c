71da3bafd6bf4f9373fdbf6663be313b93920906
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-11 15:35:46 +0000

    Fri, Feb 11, 2022, 3:35 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -19,6 +19,24 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        int r_red[width];
+        int r_green[width];
+        int r_blue[width];
+        for (int j = width - 1, n = 0; j >= 0; j--, n++)
+        {
+            r_red[n] = image[i][j].rgbtRed;
+            r_green[n] = image[i][j].rgbtGreen;
+            r_blue[n] = image[i][j].rgbtBlue;
+        }
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
     return;
 }
 
