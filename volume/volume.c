2d2a80f5727068eb5c8bfbaf08b7e3bc4c409384
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-11 09:33:55 +0000

    Fri, Feb 11, 2022, 9:33 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -49,7 +49,8 @@ int main(int argc, char *argv[])
     {
         if (i > 21)
         {
-            // buffer *= factor;
+            printf("%u\n", buffer);
+            buffer *= factor;
             printf("%u\n", buffer);
             fwrite(&buffer, sizeof(int16_t), 1, output);
         }
