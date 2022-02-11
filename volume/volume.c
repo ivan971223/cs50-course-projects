494fd5b4c9591b0afbc0378045f948e4c52e78c2
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-11 08:39:10 +0000

    Fri, Feb 11, 2022, 8:39 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,6 +36,10 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
     fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    for(int i =0; i<HEADER_SIZE; i++)
+    {
+        printf("%")
+    }
     fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
