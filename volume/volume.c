73106e1a5e42e7b13c7516f92b56e9d448f2763c
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-11 08:31:16 +0000

    Fri, Feb 11, 2022, 8:31 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
     int16_t buffer;
     while (fread(&buffer, sizeof(int16_t), 1, input))
     {
-        fwrite();
+        fwrite(&buffer, sizeof(int16_t), 1, input);
     }
 
     // Close files
