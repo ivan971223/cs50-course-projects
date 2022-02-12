53607582a17a98624969826e0555733ad8df18b0
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-12 09:35:59 +0000

    Sat, Feb 12, 2022, 9:35 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        printf("take exact one argument only");
+        return 1;
+    }
+
     FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
-        printf("The file is not readable.") return 1;
-    }
-    if (argc > 2)
-    {
-        printf("take exact one argument only");
+        printf("The file is not readable.");
         return 1;
     }
 }
