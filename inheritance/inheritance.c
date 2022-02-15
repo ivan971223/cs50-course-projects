f96ce056a8957f677f2dab89c5979449ddb58cee
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-15 12:29:57 +0000

    Tue, Feb 15, 2022, 12:29 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -53,7 +53,8 @@ person *create_family(int generations)
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
         int i = rand() % 2;
-        p->alleles[0] = ;
+        int j = rand() % 2;
+        p->alleles[0] = p;
         p->alleles[1] = ;
     }
 
