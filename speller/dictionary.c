1bd8246f7556a1f7371ec9f18698d893aeca067c
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-16 17:10:42 +0000

    Wed, Feb 16, 2022, 5:10 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -70,6 +70,7 @@ bool load(const char *dictionary)
                 {
                     while (table[j]->next != NULL)
                     {
+                        
                     }
                 }
             }
