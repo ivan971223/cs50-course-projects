974e7cab8e0021364cd6948124661f46ba7f4cfa
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-18 17:21:18 +0000

    Fri, Feb 18, 2022, 5:21 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -240,7 +240,7 @@ bool unload(void)
     for (int i = N - 1; i >= 0; i--)
     {
         node *n = table[i]->next;
-        free_memory(n);
+        // free_memory(n);
         free(table[i]);
     }
     return true;
