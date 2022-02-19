943125139e7c4f8f6b0f7df70cd3fb4b3431e1cc
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-19 14:34:50 +0000

    Sat, Feb 19, 2022, 2:34 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -30,7 +30,6 @@ bool check(const char *word)
     for (node *n = table[index]->next; n != NULL; n = n->next)
     {
         char *str = calloc(strlen(word) + 1, sizeof(char));
-        char *str2 = malloc(strlen(word)+1);
         // for (int j = 0, k = strlen(word); j < k; j++)
         // {
         //     str[j] = n->word[j];
