362778fb2e36936827ede34b09294e94b3cc0358
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-24 15:09:15 +0000

    Thu, Feb 24, 2022, 3:09 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,10 +2,16 @@
 text = input("Text: ")
 num_letters = 0
 num_words = 0
+num_sentences = 0
+
 for char in text:
     if char == " ":
         num_words += 1
+    elif char == ".":
+        num_sentences += 1
     else:
         num_letters += 1
+
 print(num_letters)
 print(num_words)
+print(num_sentences)
