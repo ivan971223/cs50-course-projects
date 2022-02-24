1f6ea55b21165275be2214ca00f6d4639bf4ca48
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-24 15:10:05 +0000

    Thu, Feb 24, 2022, 3:10 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,9 +5,10 @@ num_words = 0
 num_sentences = 0
 
 for char in text:
+    char = char.lower()
     if char == " ":
         num_words += 1
-    elif char == ".":
+    elif char == "." or char=="!" or char=="?":
         num_sentences += 1
     else:
         num_letters += 1
