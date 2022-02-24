201feb3511114928f596b88b7d2d541945698e33
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-24 15:45:35 +0000

    Thu, Feb 24, 2022, 3:45 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -11,13 +11,20 @@ for char in text:
     char = char.lower()
     if char == " ":
         num_words += 1
-    elif char == "." or char=="!" or char=="?":
+    elif char == "." or char == "!" or char == "?":
         num_sentences += 1
     elif char in alpha_ls:
         num_letters += 1
 
-index = round(0.0588 * num_letters/num_words*100 - 0.296 * num_sentences/num_words*100 - 15.8)
-print(f"Grade: {index}")
+index = round(0.0588 * num_letters/num_words*100 -
+              0.296 * num_sentences/num_words*100 - 15.8)
+if index >= 16:
+    print("Grade 16+")
+elif index < 1:
+    print("Before Grade 1")
+else:
+    print(f"Grade: {index}")
+
 # print(num_letters)
 # print(num_words)
 # print(num_sentences)
