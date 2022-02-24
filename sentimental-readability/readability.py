4c047208964f75275734cd6177f69b8cbe3a8ab6
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-24 15:18:21 +0000

    Thu, Feb 24, 2022, 3:18 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,8 +1,11 @@
 # TODO
+import string
+
 text = input("Text: ")
 num_letters = 0
 num_words = 0
 num_sentences = 0
+alpha_ls = list(string.ascii_lowercase)
 
 for char in text:
     char = char.lower()
@@ -10,7 +13,7 @@ for char in text:
         num_words += 1
     elif char == "." or char=="!" or char=="?":
         num_sentences += 1
-    elif char in ["a":"z"]:
+    elif char in alpha_ls:
         num_letters += 1
 
 print(num_letters)
