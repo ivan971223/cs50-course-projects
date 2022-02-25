83387043569d3b3c90e49ddd5df75a91a0ed5ed1
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-02-25 13:08:02 +0000

    Fri, Feb 25, 2022, 1:08 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -20,6 +20,7 @@ def main():
             break
         for row in reader:
             name_ls.append(row[0])
+        print(name_ls)
         i = 0
         for row in reader:
             dict = {}
