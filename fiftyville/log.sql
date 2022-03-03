0bd2a348bc74a320235b4088a4ba496d9f23ec3b
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-03 17:41:05 +0000

    Thu, Mar 3, 2022, 5:41 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+--To get the data of hour and minute that the thief entered at
+SELECT * FROM bakery_security_logs WHERE month = "7" and day = "28";
