87cb12933e5c451f29f031d012a00182f061ba29
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-03 15:10:21 +0000

    Thu, Mar 3, 2022, 3:10 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,1 @@
-SELECT title from movies, stars, people WHERE movies.id = stars.movie_id and stars.person_id = people.id AND name = "Johnny Depp" and name = "Helena Bonham Carter";
+SELECT title from movies, stars, people WHERE movies.id = stars.movie_id and stars.person_id = people.id AND name = "Johnny Depp" IN (SELECT id from movies, stars, people WHERE movies.id = stars.movie_id and stars.person_id = people.id AND name = "Helena Bonham Carter");
