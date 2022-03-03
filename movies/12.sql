6b288b4c71bb59d29103197c8ad3b6dd37bb2b38
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-03 14:20:05 +0000

    Thu, Mar 3, 2022, 2:20 PM +00:00

---
+++ movies/12.sql
@@ -0,0 +1,1 @@
+SELECT title from movies, stars, people WHERE movies.id = stars.movie_id and stars.person_id = people.id AND (name = "Johnny Depp" OR name = "Helena Bonham Carter");
---
+++ movies/13.sql
@@ -0,0 +1,1 @@
+SELECT name from people, movies, stars WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND name = "Kevin Bacon" AND birth = "1958";
