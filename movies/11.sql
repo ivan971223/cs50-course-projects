56f1dbcc9990d7c27ce700958b139f3133b7d9bf
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-03 14:13:10 +0000

    Thu, Mar 3, 2022, 2:13 PM +00:00

---
+++ movies/11.sql
@@ -0,0 +1,1 @@
+SELECT title from movies, stars, people, ratings WHERE movies.id = stars.movie_id AND movies.id = ratings.movie_id AND people.id = stars.person_id AND name = "Chadwick Boseman" ORDER BY rating;
