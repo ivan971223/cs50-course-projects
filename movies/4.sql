3116283a8243948cbe246fe69f897b78fd690401
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-03 13:44:40 +0000

    Thu, Mar 3, 2022, 1:44 PM +00:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(title) FROM movies WHERE id in (SELECT movie_id from ratings WHERE rating = 10.0);
+SELECT COUNT(title) FROM movies WHERE id IN (SELECT movie_id from ratings WHERE rating = 10.0);
