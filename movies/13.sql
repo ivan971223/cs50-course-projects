4bbb39c052b12d3ae6ae7b0073e72a6b1c8e60f3
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-03 14:23:09 +0000

    Thu, Mar 3, 2022, 2:23 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT id from people, movies, stars WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND name = "Kevin Bacon" AND birth = "1958";
+SELECT movies.id from people, movies, stars WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND name = "Kevin Bacon" AND birth = "1958";
