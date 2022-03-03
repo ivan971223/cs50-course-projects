00f5afadf0ded7c7c79337f5813ad59bd0964521
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-03 15:00:03 +0000

    Thu, Mar 3, 2022, 3:00 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM movies, ratings WHERE movies.id = ratings.movie_id AND ratings.movie_id IN (SELECT id FROM movies WHERE year = "2010") ORDER BY rating, title;
+SELECT title, rating FROM movies, ratings WHERE movies.id = ratings.movie_id AND ratings.movie_id IN (SELECT id FROM movies WHERE year = "2010") ORDER BY rating DESC, title;
