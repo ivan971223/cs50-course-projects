64000e15306fc456e9587f5e43cd70a6e40dedcd
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-03 17:45:03 +0000

    Thu, Mar 3, 2022, 5:45 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,3 +2,8 @@
 
 --To get the data of hour and minute that the thief entered at
 SELECT * FROM bakery_security_logs WHERE month = "7" and day = "28";
+--To get the time for duck took place in 10:15 am
+SELECT * FROM crime_scene_reports WHERE year = "2021" and month = "7" and day = "28" and street = "Humphrey Street";
+
+--interviews
+SELECT * FROM interviews WHERE year = "2021" and month = "7" and day= "28";
