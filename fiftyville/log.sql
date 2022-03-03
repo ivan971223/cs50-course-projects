cb8fbd557690eb58d9dc058f352d430c542cbf1b
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-03 19:06:10 +0000

    Thu, Mar 3, 2022, 7:06 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -39,18 +39,6 @@ SELECT * FROM flights WHERE year = "2021" and month = "7" and day = "29" and ori
 -- | 43 | 8                 | 1                      | 2021 | 7     | 29  | 9    | 30     |
 -- +----+-------------------+------------------------+------+-------+-----+------+--------+
 
---get tel no. caller : (031) 555-6622, receiver: (910) 555-3251
-SELECT * FROM phone_calls WHERE year = "2021" and month  = "7" and day = "28" and duration = 38;
-
---people, query by phone number
-SELECT * FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = "2021" and month  = "7" and day = "28" and duration = 38);
-SELECT * FROM people WHERE phone_number IN (SELECT receiver FROM phone_calls WHERE year = "2021" and month  = "7" and day = "28" and duration = 38);
--- id   |  name  |  phone_number  | passport_number | license_plate
--- 907148 | Carina | (031) 555-6622 | 9628244268      | Q12B3Z3
-
---receiver:
---712712 | Jacqueline | (910) 555-3251 |                 | 43V0R5D
-
 -- find the transactions with bank account number to get the person_id
 SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = "2021" and month  = "7" and day = "28" and transaction_type = "withdraw" and atm_location = "Leggett Street");
 --  686048    |
@@ -70,6 +58,7 @@ SELECT * FROM people WHERE phone_number IN (SELECT receiver FROM phone_calls WHE
 -- | 907148 | Carina | (031) 555-6622 | 9628244268      | Q12B3Z3       |
 
 --Taylor and Bruce match the caller table and atm withdrawal table
+-- Taylor call Larry
 
 --receiver
 -- +--------+------------+----------------+-----------------+---------------+
