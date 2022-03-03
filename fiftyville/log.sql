cbd07ae63b207c89275d78f2faa0be2c77f05d0e
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-03 18:38:18 +0000

    Thu, Mar 3, 2022, 6:38 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -55,3 +55,10 @@ SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_numb
 -- | 467400    |
 -- | 449774    |
 -- | 438727
+SELECT * FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = "2021" and month  = "7" and day = "28" and duration >=20 and duration <= 45);
+-- |   id   |  name  |  phone_number  | passport_number | license_plate |
+--  449774 | Taylor | (286) 555-6063 | 1988161715      | 1106N58       |
+-- | 560886 | Kelsey | (499) 555-9472 | 8294398571      | 0NTHK55       |
+-- | 686048 | Bruce  | (367) 555-5533 | 5773159633      | 94KL13X       |
+-- | 907148 | Carina | (031) 555-6622 | 9628244268      | Q12B3Z3       |
+SELECT * FROM people, bank_account WHERE people.id = bank_accounts.person_id AND bank_accounts.account_number IN (SELECT account_number FROM atm_transactions WHERE year = "2021" and month  = "7" and day = "28" and transaction_type = "withdraw" and atm_location = "Leggett Street");
