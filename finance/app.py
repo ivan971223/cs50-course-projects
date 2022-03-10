b7f914362b81a6a4e5f1c627f7f5916e8e755f00
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-10 18:20:19 +0000

    Thu, Mar 10, 2022, 6:20 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -43,7 +43,7 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    db.execute("SELECT DISTINCT FROM transactions")
+    db.execute("SELECT DISTINCT() FROM transactions")
     db.execute("SELECT SUM() FROM transactions")
     return render_template("index.html")
 
