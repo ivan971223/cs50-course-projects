aeba5dc92737b51ea7b5e4cf153b208e5edafed4
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-10 17:59:25 +0000

    Thu, Mar 10, 2022, 5:59 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -59,7 +59,8 @@ def buy():
         if not shares.is_integer() or shares <= 0:
             return apology("Invalid number of shares")
         price = qdict["price"]
-        cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[""]
+        rows = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
+        cash = rows[0]["cash"]
         date = datetime.datetime.now()
         year = date.year
         month = date.month
