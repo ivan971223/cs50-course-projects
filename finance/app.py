970870db434369bb8da7dce12a5c78fff3060200
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-11 09:31:10 +0000

    Fri, Mar 11, 2022, 9:31 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -88,7 +88,7 @@ def buy():
             db.execute("INSERT INTO transactions (user_id, date, symbol, price, shares) VALUES(?,?,?,?,?)", session["user_id"], date, symbol, price, shares)
             cash -= price * shares
             db.execute("UPDATE users SET cash = ? WHERE id = ?", cash, session["user_id"])
-            return render_template("index.html")
+            return redirect("/")
     else:
         return render_template("buy.html")
 
