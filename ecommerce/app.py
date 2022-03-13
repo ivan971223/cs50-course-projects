ed9d973e10d8551f7144aee1ee477a3ccf09be1f
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-13 20:17:24 +0000

    Sun, Mar 13, 2022, 8:17 PM +00:00

--- ecommerce/app.py
+++ ecommerce/app.py
@@ -37,7 +37,7 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    foods = db.execute("SELECT name, price FROM food")
+    foods = db.execute("SELECT id, name, price FROM food")
     return render_template("index.html", foods = foods)
 
 
