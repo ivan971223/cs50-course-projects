36791f7778ef2779fb235fc0e3851638cbeb3ff1
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-13 18:42:33 +0000

    Sun, Mar 13, 2022, 6:42 PM +00:00

--- ecommerce/database.py
+++ ecommerce/database.py
@@ -1,13 +1,13 @@
 import cs50
 import csv
 
-open("food.db","w").close()
+open("shop.db","w").close()
 db = cs50.SQL("sqlite:///food.db")
 
 db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC NOT NULL DEFAULT 1000.00)")
 db.execute("CREATE TABLE food (id INTEGER, name TEXT NOT NULL, price REAL NOT NULL, PRIMARY KEY(id))")
 
-with open("food.csv", "r") as file:
+with open("shop.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         name = row["name"].strip()
