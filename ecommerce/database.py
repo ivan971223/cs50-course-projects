a95524ea17637418fb7cb58dbf3bc3075a132c13
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-13 18:45:59 +0000

    Sun, Mar 13, 2022, 6:45 PM +00:00

--- ecommerce/database.py
+++ ecommerce/database.py
@@ -2,7 +2,7 @@ import cs50
 import csv
 
 open("shop.db","w").close()
-db = cs50.SQL("sqlite:///food.db")
+db = cs50.SQL("sqlite:///shop.db")
 
 db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC NOT NULL DEFAULT 1000.00)")
 db.execute("CREATE TABLE food (id INTEGER, name TEXT NOT NULL, price REAL NOT NULL, PRIMARY KEY(id))")
