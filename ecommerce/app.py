aeb95173e0647752c5bd7295e5a0439f1525f0bc
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-14 17:29:42 +0000

    Mon, Mar 14, 2022, 5:29 PM +00:00

--- ecommerce/app.py
+++ ecommerce/app.py
@@ -103,6 +103,7 @@ def order():
             number = session["cart"][f"{food_id}"]
             order_date = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
             db.execute("INSERT INTO order (orderDate, user_id, food_id, number) VALUES(?,?,?,?)", order_date, session["user_id"], food_id, number)
+            
     orders = db.execute("SELECT * FROM order WHERE user_id = ?",session["user_id"])
     return render_template("order.html")
 
