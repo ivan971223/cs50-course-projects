57fed8d7577d32f078e1496c6868822344c70cbe
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-14 08:08:12 +0000

    Mon, Mar 14, 2022, 8:08 AM +00:00

--- ecommerce/database.py
+++ ecommerce/database.py
@@ -6,7 +6,7 @@ db = cs50.SQL("sqlite:///shop.db")
 
 db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC NOT NULL DEFAULT 1000.00)")
 db.execute("CREATE TABLE food (id INTEGER, name TEXT NOT NULL, price REAL NOT NULL, PRIMARY KEY(id))")
-db.execute("CREATE TABLE cart (user_id INTEGER, name TEXT NOT NULL, FORIEGN KEY(user_id) REFERENCES users(id)")
+db.execute("CREATE TABLE cart (user_id INTEGER, name TEXT NOT NULL, price REAL NOT NULL, FORIEGN KEY(user_id) REFERENCES users(id)")
 with open("shop.csv", "r", encoding='utf-8-sig') as file:
     reader = csv.DictReader(file)
     for row in reader:
