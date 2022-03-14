fb31c9ec8f6d223b8a65e05af41dc353eb2b34b4
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-14 11:38:33 +0000

    Mon, Mar 14, 2022, 11:38 AM +00:00

--- ecommerce/app.py
+++ ecommerce/app.py
@@ -73,7 +73,7 @@ def cart():
                 food["price"] = usd(food["price"])
                 index += 1
             total = usd(total)
-            return render_template("cart.html", foods=foods, total=total, success=True)
+            return render_template("cart.html", foods=foods, total=total, is_added=True)
         elif action == "delete":
             session["cart_item"].remove(id)
             session["cart"].pop(f"{id}")
@@ -90,7 +90,7 @@ def cart():
         food["price"] = usd(food["price"])
         index += 1
     total = usd(total)
-    return render_template("cart.html", foods=foods, total=total, success=False)
+    return render_template("cart.html", foods=foods, total=total, is_added=False)
 
 
 # @app.route("/history")
