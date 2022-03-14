596b2dc8f1f1bc53e9708a45b157340d5c0c53ee
ivan971223 <39795556+ivan971223@users.noreply.github.com>
2022-03-14 18:31:26 +0000

    Mon, Mar 14, 2022, 6:31 PM +00:00

--- ecommerce/app.py
+++ ecommerce/app.py
@@ -114,7 +114,7 @@ def order():
     orders = db.execute(
         "SELECT * FROM orders WHERE user_id = ? GROUP BY orderDate", session["user_id"])
     for order in orders:
-
+        db.execute("")order[id]
     return render_template("order.html", orders=orders)
 
 
